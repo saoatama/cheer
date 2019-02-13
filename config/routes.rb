4af3e9c53dd3506d02fd15f161e9e3f63fa23d20
EC2 Default User <ec2-user@ip-172-31-44-172.us-east-2.compute.internal>
2019-02-13 13:23:07 +0000

    fixed root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :blogs
-  get "cocktail" => "cocktail#index"
+  root to: "cocktail#index"
   post "cocktail/post" => "cocktail#post"
   get "cocktail/post" => "cocktail#post"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
