4308b8ffc70277342e99449b56779a497ea7dc16
EC2 Default User <ec2-user@ip-172-31-44-172.us-east-2.compute.internal>
2019-02-14 03:05:11 +0000

    fixed 条件分岐を乱数に変更

--- app/controllers/cocktail_controller.rb
+++ app/controllers/cocktail_controller.rb
@@ -8,55 +8,10 @@ class CocktailController < ApplicationController
     end
     
     def post
+        @array = ["ジントニック", "ピーチウーロン", "カシスオレンジ", "ファジーネーブル", "モスコミュール", "マティーニ", "レッドアイ", "ベッドウィーン・ザ・シーツ", "ルシアン","カルーアミルク","青いサンゴ礁","アレキサンダー","ロングアイランドアイスティー","セックスオンザビーチ","キッスインザダーク"]
+        @cocktail = @array.sort_by{rand}
         @active = params[:active].to_i
         @pass = params[:passive].to_i
-        @mix = @active*@pass
-        @result =
-        if @active > @pass
-            if @mix%12===0
-                "ジントニック"
-            elsif @mix > 100
-                "ホワイトレディ"
-            elsif @mix%6===0
-                "ピーチウーロン"
-            elsif @mix%8===0
-                "カシスオレンジ"
-            elsif @mix%2===0
-                "ファジーネーブル"
-            elsif @mix%5===0
-                "モスコミュール"
-            elsif @mix%3===0
-                "マティーニ"
-            else
-                "レッドアイ"
-            end
-        elsif @active < @pass
-            if @mix%10===0
-                "ピンクレディ"
-            elsif @mix >= 100
-                "ビトウィーン・ザ・シーツ"
-            elsif @mix%6===0
-                "ルシアン"
-            elsif @mix%8===0
-                "スクリュードライバー"
-            elsif @mix%2===0
-                "カルーアミルク"
-            elsif @mix%5===0
-                "青い珊瑚礁"
-            elsif @mix%3===0
-                "アレキサンダー"
-            else
-                "ダイキリ"
-            end
-        else
-            if @mix > 100
-                "ロングアイランドアイスティー"
-            elsif @mix > 12
-                "セックス・オン・ザ・ビーチ"
-            else
-                "キッス・イン・ザ・ダーク"
-            end
-                    
-        end
+        @result = @cocktail[0]
     end
 end
