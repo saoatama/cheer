ebad70a8e81f1ae261a3f03ae35d2fc89614a268
saoatama <riotgarnet21@gmail.com>
2019-02-16 16:25:20 +0000

    fixed ルートをまとめた

--- app/controllers/quote_controller.rb
+++ app/controllers/quote_controller.rb
@@ -2,15 +2,7 @@ class QuoteController < ApplicationController
     def index
         @result = "#{params[:name]}『#{params[:title]}』(#{params[:publisher]}, #{params[:year]}) 。"
         @result2 = "#{params[:name]}『#{params[:title]}ー#{params[:subtitle]}』(#{params[:publisher]}, #{params[:year]}) 。"
-
-    end
-    
-    def essay
-        @result = "#{params[:book_author]}「#{params[:book_title]}」#{params[:editor]}編『#{params[:book_title]}』(#{params[:publisher]}, #{params[:year]})。"
-    end
-    
-    def translation
-        @result = "#{params[:author]}(#{params[:translator]}訳)『#{params[:title]}』(#{params[:publisher]}, #{params[:year]}) 。"
-
+        @result3 = "#{params[:book_author]}「#{params[:book_title]}」#{params[:editor]}編『#{params[:book_title]}』(#{params[:publisher]}, #{params[:year]})。"
+        @result4 = "#{params[:author]}(#{params[:translator]}訳)『#{params[:title]}』(#{params[:publisher]}, #{params[:year]}) 。"
     end
 end
