5627748394949b7d6095bebe7ca144c55974eeba
saoatama <riotgarnet21@gmail.com>
2019-02-16 16:50:49 +0000

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "quote#index"
   post "/" => "quote#index"
-  get "cocktail" => "cokctail#index"
+  get "cocktail" => "cocktail#index"
   post "cocktail/post" => "cocktail#post"
   get "cocktail/post" => "cocktail#post"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
